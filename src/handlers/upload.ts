fdaf56831b0abfac49d0ac1faeca663e6e194139
Felix Yadomi <dev.yadomi@gmail.com>
2019-04-06 21:06:25 +0200

    Insert media information with mongo when uploading a media

--- src/handlers/upload.ts
+++ src/handlers/upload.ts
@@ -1,31 +1,38 @@
-import { Request, ResponseToolkit } from 'hapi';
-import uuidv4 from 'uuid/v4';
-import fileType from 'file-type';
-import Boom from 'boom';
+import { Request, ResponseToolkit } from "hapi"
+import uuidv4 from "uuid/v4"
+import fileType from "file-type"
+import Boom from "boom"
 
-import { Readable } from 'stream';
-import IO from '../helpers/io';
+import { Readable } from "stream"
+import IO from "../helpers/io"
 
-const isSupported = (mimetype: string) => ['image/jpeg', 'image/webp', 'image/png'].includes(mimetype);
+const isSupported = (mimetype: string) => ["image/jpeg", "image/webp", "image/png"].includes(mimetype)
 
 module.exports = async (request: Request, h: ResponseToolkit) => {
-  // @ts-ignore
-  const data: Readable = request.payload.data;
+	// @ts-ignore
+	const data: Readable = request.payload.data
 
-  const readStream = await fileType.stream(data);
-  if (!readStream || (readStream && !readStream.fileType)) {
-    throw Boom.badData();
-  }
+	const readStream = await fileType.stream(data)
+	if (!readStream || (readStream && !readStream.fileType)) {
+		throw Boom.badData()
+	}
 
-  if (readStream.fileType && readStream.fileType.mime && !isSupported(readStream.fileType.mime)) {
-    throw Boom.unsupportedMediaType();
-  }
+	if (readStream.fileType && readStream.fileType.mime && !isSupported(readStream.fileType.mime)) {
+		throw Boom.unsupportedMediaType()
+	}
 
-  const uuid = uuidv4();
-  await IO.write(uuid, readStream);
+	const uuid = uuidv4()
+	await IO.write(uuid, readStream)
 
-  return {
-    uuid,
-    createdAt: new Date(),
-  };
-};
+	const metadata = {
+		uuid,
+		createdAt: new Date().toISOString()
+	}
+	const db = request.server.app.mongo.db
+	await db.collection("media").insertOne(metadata)
+
+	return {
+		uuid,
+		createdAt: new Date()
+	}
+}
