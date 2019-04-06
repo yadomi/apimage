ec4aaa89ed0f5515f329e5e0b87c4161738b66d3
Felix Yadomi <dev.yadomi@gmail.com>
2019-04-06 21:04:40 +0200

    Register database module and prettier rewrite

--- src/index.ts
+++ src/index.ts
@@ -1,47 +1,52 @@
-const Hapi = require('hapi');
+const Hapi = require("hapi")
 
 export const server = Hapi.server({
   port: 3000,
-  host: '0.0.0.0',
-  debug: { request: ['error'] },
-});
+  host: "0.0.0.0",
+  debug: { request: ["error"] }
+})
 
 server.route({
-  method: 'GET',
-  path: '/{query}/{uuid}',
-  handler: require('./handlers/read'),
+  method: "GET",
+  path: "/{query}/{uuid}",
+  handler: require("./handlers/read"),
   options: {
-    validate: require('./validators/read'),
-  },
-});
+    validate: require("./validators/read")
+  }
+})
 
 server.route({
-  method: 'POST',
-  path: '/',
-  handler: require('./handlers/upload'),
+  method: "POST",
+  path: "/",
+  handler: require("./handlers/upload"),
   options: {
-    validate: require('./validators/upload'),
+    validate: require("./validators/upload"),
     payload: {
-      output: 'stream',
+      output: "stream",
       parse: true,
-      allow: 'multipart/form-data',
-      maxBytes: 1000 * 1000 * 1500, // 15 MB
-    },
-  },
-});
+      allow: "multipart/form-data",
+      maxBytes: 1000 * 1000 * 1500 // 15 MB
+    }
+  }
+})
 
 const main = async () => {
-  await server.start();
-  console.log(`Server running at: ${server.info.uri}`);
-};
+  // prettier-ignore
+  await server.register([
+    require('./modules/database/database').default,
+  ]);
 
-process.on('unhandledRejection', err => {
-  console.error(err);
-  process.exit(1);
-});
+  await server.start()
+  console.log("[Hapi]", `Server running at: ${server.info.uri}`)
+}
 
-process.on('SIGINT', () => {
-  process.exit(0);
-});
+process.on("unhandledRejection", err => {
+  console.error(err)
+  process.exit(1)
+})
 
-main();
+process.on("SIGINT", () => {
+  process.exit(0)
+})
+
+main()
