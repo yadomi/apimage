43b24d699e26be1c930dff039935bd7d1faf0c70
Felix Yadomi <dev.yadomi@gmail.com>
2019-04-06 22:55:19 +0200

    Apply formatting on index

--- src/index.ts
+++ src/index.ts
@@ -1,52 +1,52 @@
 const Hapi = require("hapi")
 
 export const server = Hapi.server({
-  port: 3000,
-  host: "0.0.0.0",
-  debug: { request: ["error"] }
+	port: 3000,
+	host: "0.0.0.0",
+	debug: { request: ["error"] }
 })
 
 server.route({
-  method: "GET",
-  path: "/{query}/{uuid}",
-  handler: require("./handlers/read"),
-  options: {
-    validate: require("./validators/read")
-  }
+	method: "GET",
+	path: "/media/{query}/{uuid}",
+	handler: require("./handlers/read"),
+	options: {
+		validate: require("./validators/read")
+	}
 })
 
 server.route({
-  method: "POST",
-  path: "/",
-  handler: require("./handlers/upload"),
-  options: {
-    validate: require("./validators/upload"),
-    payload: {
-      output: "stream",
-      parse: true,
-      allow: "multipart/form-data",
-      maxBytes: 1000 * 1000 * 1500 // 15 MB
-    }
-  }
+	method: "POST",
+	path: "/media",
+	handler: require("./handlers/upload"),
+	options: {
+		validate: require("./validators/upload"),
+		payload: {
+			output: "stream",
+			parse: true,
+			allow: "multipart/form-data",
+			maxBytes: 1000 * 1000 * 1500 // 15 MB
+		}
+	}
 })
 
 const main = async () => {
-  // prettier-ignore
-  await server.register([
+	// prettier-ignore
+	await server.register([
     require('./modules/database/database').default,
   ]);
 
-  await server.start()
-  console.log("[Hapi]", `Server running at: ${server.info.uri}`)
+	await server.start()
+	console.log("[Hapi]", `Server running at: ${server.info.uri}`)
 }
 
 process.on("unhandledRejection", err => {
-  console.error(err)
-  process.exit(1)
+	console.error(err)
+	process.exit(1)
 })
 
 process.on("SIGINT", () => {
-  process.exit(0)
+	process.exit(0)
 })
 
 main()
