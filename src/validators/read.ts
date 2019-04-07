1b949604833ec01fabd2d0b07c1ca15aef013863
Felix Yadomi <dev.yadomi@gmail.com>
2019-04-07 18:01:28 +0200

    Pass publicId in validator for authentication

--- src/validators/read.ts
+++ src/validators/read.ts
@@ -1,10 +1,11 @@
-import Joi from 'joi';
+import Joi from "joi"
 
 module.exports = {
-  params: {
-    query: Joi.string().regex(/[a-z0-9,_]/),
-    uuid: Joi.string()
-      .regex(/[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}(?:\.[a-z]{3,4})?$/)
-      .required(),
-  },
-};
+	params: {
+		publicId: Joi.string().regex(/^[a-zA-Z0-9]{12}$/),
+		query: Joi.string().regex(/[a-z0-9,_]/),
+		uuid: Joi.string()
+			.regex(/[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}(?:\.[a-z]{3,4})?$/)
+			.required()
+	}
+}
